eda359a5b3844a4dfd82f91e7327e77c23a546b7
fuzionzeak <fuzionzeak@gmail.com>
2024-10-03 14:52:12 +0200

    modification du css

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
 <body>
 
 <!-- Liens vers les différentes pages contrôleur -->
-<nav>
-    <ul>
-        <li><a href="index.php?page=page1">Liste des clubs</a></li>
-        <li><a href="index.php?page=page2">S'inscrire</a></li>
-        <li><a href="index.php?page=page3">Voir Page 3</a></li>
-    </ul>
-</nav>
+<header>
+    <nav>
+        <ul class="menu">
+            <li><a href="index.php?page=page1">Liste des clubs</a></li>
+            <li><a href="index.php?page=page2">S'inscrire</a></li>
+            <li><a href="index.php?page=page3">Voir Page 3</a></li>
+        </ul>
+    </nav>
+</header>
 
 <!-- Inclusion du routeur pour gérer les différentes pages -->
 <?php

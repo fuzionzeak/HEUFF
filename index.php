42ad1d4a2e39c27072b5bbdccbc2fabe720ec015
Anthony <127119364+fuzionzeak@users.noreply.github.com>
2024-10-10 17:22:35 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -28,3 +29,35 @@ require_once 'router.php';
 
 </body>
 </html>
+=======
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="/HEUFF/css/style.css">
+</head>
+<body>
+
+<!-- Liens vers les différentes pages contrôleur -->
+<header>
+    <nav>
+    <div class="logo">
+        <img src="/HEUFF/images/Logo_Ligue1.png" alt="Logo Ligue 1">
+    </div>
+        <ul class="menu">
+            <li><a href="index.php?page=page1">Liste des clubs</a></li>
+            <li><a href="index.php?page=page2">S'inscrire</a></li>
+            <li><a href="index.php?page=page3">Se connecter</a></li>
+        </ul>
+    </nav>
+</header>
+
+<!-- Inclusion du routeur pour gérer les différentes pages -->
+<?php
+require_once 'router.php';
+?>
+
+</body>
+</html>
+>>>>>>> 45817e33f25d270d1a9a29bf228b1b53a0ac5675
